15355fff497aca1387bbffd7fdd276d757ff5d05
oPOCCOMAXAo <nikita_kurd_ukr@mail.ru>
2016-05-03 21:02:22 +0300

    Added files via upload

--- v4/engine.js
+++ v4/engine.js
@@ -16,15 +16,7 @@ var rmin = 0.0001; // минимально допустимое расстоян
 var r2min = rmin * rmin;
 var MAX_DIST2 = MAX_DIST * MAX_DIST;
 
-function Body(x, y) {
-    this.t = 0.0;          // температура
-    this.m = M0;           // масса
-    this.r = R0;            // радиус
-    this.x = x;            // координата
-    this.y = y;            // координата
-    this.vx = -0.01 * x;   // скорость
-    this.vy = -0.01 * y;   // скорость
-}
+var param;
 
 addEventListener("message", function (e) {
     var data = e.data;
@@ -34,12 +26,15 @@ addEventListener("message", function (e) {
             INTERVAL = data.interval;
             dt = INTERVAL * 0.001;
             body = [];
+            param = new Array(MAX);
+            for (var i = 0; i < MAX; i++)
+                param[i] = new Array(MAX);
             addInitBody(MAX, INIT_DIST * Math.sqrt(MAX));
             postMessage(body);
             break;
         case "start":
             if (timer == null)
-                timer = setInterval(timered, INTERVAL);
+                timer = setInterval(step, INTERVAL);
             break;
         case "pause":
             if (timer != null) {
@@ -50,44 +45,81 @@ addEventListener("message", function (e) {
     }
 });
 
-function step() {
-    var ai, aj, ax, ay, dx, dy, r, r2, rr, i, j, de, dm, u1 = {}, u2 = {}, u3 = {}, u4 = {};
-    addNewBody(MAX - body.length, AVG_DIST);
+function Body(x, y) {
+    this.t = 0.0;          // температура
+    this.m = M0;           // масса
+    this.r = R0;            // радиус
+    this.x = x;            // координата
+    this.y = y;            // координата
+    this.vx = -0.01 * x;   // скорость
+    this.vy = -0.01 * y;   // скорость
+}
 
-    var len = body.length;
+function IJparam(i, j) {
+    var dx, dy, r, r2, rr, sin, cos;
+    dx = body[j].x - body[i].x;
+    dy = body[j].y - body[i].y;
+    r2 = dx * dx + dy * dy;
+    if (r2 < r2min) r = r2min;
+    r = Math.sqrt(r2);
+    rr = 1.0 / r;
+    cos = dx * rr;
+    sin = dy * rr;
+    param[i][j] = {
+        dx: dx,
+        dy: dy,
+        r: r,
+        r2: r2,
+        rr: rr,
+        rr2: 1.0 / r2,
+        cos: cos,
+        sin: sin
+    };
+}
 
-    // dist
-    dist = new Array(len);
-    for (i = 0; i < len; i++) {
-        dist[i] = new Array(len);
-        for (j = i + 1; j < len; j++) {
-            dx = body[j].x - body[i].x;
-            dy = body[j].y - body[i].y;
-            r2 = dx * dx + dy * dy;
-            if (r2 < r2min) r = r2min;
-            r = Math.sqrt(r2);
-            rr = 1.0 / r;
-            ax = dx * rr;
-            ay = dy * rr;
-            dist[i][j] = {
-                dx: dx,
-                dy: dy,
-                r: r,
-                r2: r2,
-                rr: rr,
-                rr2: 1.0 / r2,
-                sumr: body[j].r + body[i].r,
-                cos: ax,
-                sin: ay
-            };
+function calcparam(indexes) {
+    var i, j;
+    if (indexes == null)
+        for (i = 0; i < MAX; i++)
+            for (j = i + 1; j < MAX; j++)
+                IJparam(i, j);
+    else
+        for (i = 0, len = indexes.length; i < len; i++)
+            for (j = 0; j < MAX; j++)
+                if (indexes[i] != j)
+                    IJparam(indexes[i], j);
+}
+
+function calcpos() {
+    var ai, aj, r;
+    for (var i = 0; i < MAX; i++)
+        for (var j = i + 1; j < MAX; j++) {
+            ai = G * body[i].m * param[i][j].rr2 * dt;
+            aj = -G * body[j].m * param[i][j].rr2 * dt;
+            body[i].vx += ai * param[i][j].cos;
+            body[i].vy += ai * param[i][j].sin;
+            // другой знак
+            body[j].vx += aj * param[i][j].cos;
+            body[j].vy += aj * param[i][j].sin;
+        }
+    for (i = 0; i < MAX; i++) {
+        body[i].x += body[i].vx * dt;
+        body[i].y += body[i].vy * dt;
+        r = body[i].x * body[i].x + body[i].y * body[i].y;
+        if (r > MAX_DIST2 || r < -MAX_DIST2) {
+            body[i].m = 0;
+            body[i].r = 0;
         }
     }
+}
 
-    // touch
-    for (i = 0; i < len; i++)
-        for (j = i + 1; j < len; j++) {
+function calctouch() {
+    var de, dm, ei, ej, v, sumr, u1 = {}, u2 = {}, u3 = {}, u4 = {};
+    for (var i = 0; i < MAX; i++)
+        for (var j = i + 1; j < MAX; j++) {
             if (body[i].m == 0 || body[j].m == 0) continue;
-            if (dist[i][j].r < dist[i][j].sumr) {
+            sumr = body[j].r + body[i].r;
+            if (param[i][j].r < sumr) {
                 if (body[i].t + body[j].t >= T_CRIT) {
                     // неупругое
                     de = body[i].m * body[i].t + body[j].m * body[j].t;
@@ -102,23 +134,20 @@ function step() {
                     body[i].r = radius(dm, body[i].t);
                 } else {
                     // упругое
-                    // ai = m v^2
-                    ai = body[i].m * (body[i].vx * body[i].vx + body[i].vy * body[i].vy);
-                    aj = body[j].m * (body[j].vx * body[j].vx + body[j].vy * body[j].vy);
-                    body[i].t += aj * KT / body[i].m;
-                    body[j].t += ai * KT / body[j].m;
+                    // E = m v^2
+                    ei = body[i].m * (body[i].vx * body[i].vx + body[i].vy * body[i].vy);
+                    ej = body[j].m * (body[j].vx * body[j].vx + body[j].vy * body[j].vy);
+                    body[i].t += ej * KT / body[i].m;
+                    body[j].t += ei * KT / body[j].m;
 
-                    dx = dist[i][j].dx;
-                    dy = dist[i][j].dy;
-
-                    var vr = (body[i].vx * dx + body[i].vy * dy) * dist[i][j].rr2;
-                    u1.x = dx * vr;
-                    u1.y = dy * vr;
+                    v = (body[i].vx * param[i][j].dx + body[i].vy * param[i][j].dy) * param[i][j].rr2;
+                    u1.x = param[i][j].dx * v;
+                    u1.y = param[i][j].dy * v;
                     u2.x = body[i].vx - u1.x;
                     u2.y = body[i].vy - u1.y;
-                    vr = (body[j].vx * dx + body[j].vy * dy) * dist[i][j].rr2;
-                    u3.x = dx * vr;
-                    u3.y = dy * vr;
+                    v = (body[j].vx * param[i][j].dx + body[j].vy * param[i][j].dy) * param[i][j].rr2;
+                    u3.x = param[i][j].dx * v;
+                    u3.y = param[i][j].dy * v;
                     u4.x = body[j].vx - u3.x;
                     u4.y = body[j].vy - u3.y;
                     body[i].vx = (u2.x + u3.x) * K;
@@ -126,58 +155,42 @@ function step() {
                     body[j].vx = (u1.x + u4.x) * K;
                     body[j].vy = (u1.y + u4.y) * K;
 
-                    // r2 = 1 / (r1 + r2)
-                    r2 = 1.0 / dist[i][j].sumr;
+                    sumr = 1.0 / sumr;
                     // u1 - точка касания
-                    u1.x = (body[i].x * body[j].r + body[j].x * body[i].r) * r2;
-                    u1.y = (body[i].y * body[j].r + body[j].y * body[i].r) * r2;
-                    body[i].x = u1.x - (body[i].r + rmin) * dist[i][j].cos;
-                    body[i].y = u1.y - (body[i].r + rmin) * dist[i][j].sin;
-                    body[j].x = u1.x + (body[j].r + rmin) * dist[i][j].cos;
-                    body[j].y = u1.y + (body[j].r + rmin) * dist[i][j].sin;
+                    u1.x = (body[i].x * body[j].r + body[j].x * body[i].r) * sumr;
+                    u1.y = (body[i].y * body[j].r + body[j].y * body[i].r) * sumr;
+                    body[i].x = u1.x - (body[i].r + rmin) * param[i][j].cos;
+                    body[i].y = u1.y - (body[i].r + rmin) * param[i][j].sin;
+                    body[j].x = u1.x + (body[j].r + rmin) * param[i][j].cos;
+                    body[j].y = u1.y + (body[j].r + rmin) * param[i][j].sin;
                     body[i].r = radius(body[i].m, body[i].t);
                     body[j].r = radius(body[j].m, body[j].t);
                 }
-                return 1;
+                calcparam([i, j]);
             }
         }
+}
 
-    // accel
-    for (i = 0; i < len; i++)
-        for (j = i + 1; j < len; j++) {
-            ai = G * body[i].m * dist[i][j].rr2 * dt;
-            aj = -G * body[j].m * dist[i][j].rr2 * dt;
-            body[i].vx += ai * dist[i][j].cos;
-            body[i].vy += ai * dist[i][j].sin;
-            // другой знак
-            body[j].vx += aj * dist[i][j].cos;
-            body[j].vy += aj * dist[i][j].sin;
-        }
+function step() {
+    // param
+    calcparam();
 
-    // move
-    for (i = 0; i < len; i++) {
-        body[i].x += body[i].vx * dt;
-        body[i].y += body[i].vy * dt;
-        var t = body[i].x * body[i].x + body[i].y * body[i].y;
-        if (t > MAX_DIST2 || t < -MAX_DIST2) {
-            body[i].m = 0;
-            body[i].r = 0;
-        }
-    }
+    // touch
+    calctouch();
 
-    for (i = 0; i < len; i++)
+    // accel and move
+    calcpos();
+
+    // remove
+    for (var i = 0, len = MAX; i < len; i++)
         if (body[i].m == 0) {
             body[i] = body[len - 1];
             body.splice(len - 1, 1);
             len--;
         }
+    addNewBody(MAX - body.length, AVG_DIST);
 
     postMessage(body);
-    return 0;
-}
-
-function timered() {
-    while (step()) continue;
 }
 
 function radius(m, t) {
